7b35cfb862734e824b60cc0dd0121e8c329bbe94
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-10-27 20:32:59 +0600

    qr code read done

--- src/components/GenerateQR.jsx
+++ src/components/GenerateQR.jsx
@@ -18,17 +18,19 @@ const GenerateQR = () => {
             <h1 className="text-2xl font-semibold text-green-500 mb-8">Generate QR code with text or url</h1>
             <form onSubmit={handleQRGenerate}>
                 <div className="flex items-center">
-                    <input name="text" type="text" placeholder="Enter text or url..." required className="w-full text-lg text-blue-500 px-4 outline-none py-2" style={{ border: "1px solid blue" }} />
+                    <input onKeyDown={() => setQrImg("")} name="text" type="text" placeholder="Enter text or url..." required className="w-full text-lg text-blue-500 px-4 outline-none py-2" style={{ border: "1px solid blue" }} />
                     <button className="px-4 py-3  bg-blue-500 text-white uppercase outline-none hover:opacity-80 transition duration-200">Generate</button>
                 </div>
             </form>
 
             {
-                qrImg && 
-                    <div className='mt-12 text-center'>
-                        <h2 className='text-green-500 text-lg text-center font-semibold'>Your generated QR Code!!</h2>
+                qrImg &&
+                <div className='mt-12 text-center'>
+                    <h2 className='text-green-500 text-lg text-center font-semibold'>Your generated QR Code!!</h2>
+                    <a href={qrImg} download={"qr.png"}>
                         <img src={qrImg} alt="QR code" className='block mx-auto w-[200px]' />
-                    </div>
+                    </a>
+                </div>
             }
         </div>
     );
--- src/components/ReadQR.jsx
+++ src/components/ReadQR.jsx
@@ -1,13 +1,28 @@
+import QrScanner from "qr-scanner";
+import { useState } from "react";
+
 const ReadQR = () => {
+    const [text, setText] = useState('')
+    const handleReadQRCode = async (e) => {
+        e.preventDefault()
+        const file = e.target.file.files[0]
+        try {
+            const result = await QrScanner.scanImage(file)
+            setText(result)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className="px-8">
             <h1 className="text-2xl font-semibold text-green-500 mb-8">Generate QR code with text or url</h1>
-            <form>
+            <form onSubmit={handleReadQRCode}>
                 <div className="flex items-center">
-                    <input type="file" placeholder="Upload QR code" required />
+                    <input name="file" type="file" placeholder="Upload QR code" accept=".png, .jpg, .jpeg" required />
                     <button className="px-4 py-3  bg-blue-500 text-white uppercase outline-none hover:opacity-80 transition duration-200">Read</button>
                 </div>
             </form>
+            {text && <h3 className="font-semibold mt-8 text-sm">Data: <a href={text} target="_blank" rel="noreferrer" title="go to the link" className="text-green-500">{text}</a></h3>}
         </div>
     );
 };
